25d4387a2ad0b40bb2e4239fc268de3f2b29c412
Jovi <poethelasi@126.com>
2016-12-14 10:54:49 +0800

    Update PDFCreateNewLine.java

--- java/pdf/PDFCreateNewLine.java
+++ java/pdf/PDFCreateNewLine.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.interactive.action.PDActionGoTo;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageXYZDestination;
 
 public class PDFMergeUtil {
 
@@ -15,7 +18,7 @@ public class PDFMergeUtil {
 
 	public static void main(String[] args) throws IOException {
 		PDDocument document = new PDDocument();
-		PDPage page = new PDPage();
+		PDPage page = new PDPage(PDRectangle.A4);// 设置页面大小,坐标(0,0)的位置在左下角
 		document.addPage(page);
 
 		PDFont font = PDType1Font.HELVETICA_BOLD;
@@ -25,7 +28,7 @@ public class PDFMergeUtil {
 		String message = "Hello.World------";
 		contentStream.beginText();
 		contentStream.setFont(font, 12);
-		contentStream.newLineAtOffset(100, 700);
+		contentStream.newLineAtOffset(0, 800);
 		for (int i = 0; i < 10000; i++) {
 			contentStream.showText(message + " " + Integer.toString(i));
 			System.out.println(message + " " + Integer.toString(i));
@@ -35,6 +38,15 @@ public class PDFMergeUtil {
 		contentStream.endText();
 
 		contentStream.close();
+
+		// 设置缩放比例:100% 75% 等等
+		PDPageXYZDestination dest = new PDPageXYZDestination();
+		dest.setPage(page);
+		dest.setZoom(1f);
+		dest.setTop(new Float(PDRectangle.A4.getHeight()).intValue());
+		PDActionGoTo action = new PDActionGoTo();
+		action.setDestination(dest);
+		document.getDocumentCatalog().setOpenAction(action);
 
 		document.save("c:\\Temp\\Hello World.pdf");
 		document.close();
